08c1e75fe210d15b027ef49a4951745e9c2ddf9b
DevenPerkins <deven.s.perkins@gmail.com>
2021-02-24 18:01:14 -0800

    aybe get this on the webz

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasureLocation: null,
+      bombLocation: null,
+      gameOver: false
+    }
+  }
+
+  componentDidMount() {
+    let treasure = Math.floor(Math.random() * 
+    this.state.board.length)
+    this.setState({ treasureLocation: treasure })
+    let bomb = Math.floor(Math.random() * 
+    this.state.board.length)
+    this.setState({ bombLocation: bomb })
+}
+  
+  handleGamePlay = (index) => {
+    const { board , treasureLocation ,bombLocation } = this.state
+    if(index === treasureLocation){
+      board[index] = "💎"
+      this.setState({ board: board, gameOver:"win"})
+    }else if(index === bombLocation){
+      board[index]= "💣"
+      this.setState({ board : board})
+    }else{
+      board[index] = "🌴"
+      this.setState({ board: board})
     }
   }
 
   render(){
+      console.log("treasure:" , this.state.treasureLocation)
+      console.log("bomb:" , this.state.bombLocation)
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className = "gameboard">
+        { this.state.board.map((value , index) => {
+          return(
+            <Square 
+              value={value}
+              key={index}
+              index ={index}
+              handleGamePlay={ this.handleGamePlay }
+            />
+          )
+        })}
+        </div>
+        {
+          this.state.gameOver &&
+          <p className= "message">🎉Winner Winner Chicken Dinner🎉</p>
+
+        }
       </>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+
+  handleClick = () => {
+    this.props.handleGamePlay(this.props.index)
+  }
+
   render(){
     return(
       <>
+       <div className= 'square' onClick={this.handleClick}>{ this.props.value }</div>
       </>
     )
   }
